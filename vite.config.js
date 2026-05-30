94817320e49a83584617c6ea87f9b8002ed2f2b0
shahhelly1307-arch <shahhelly1307@gmail.com>
2026-05-30 18:53:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   base: './',
-  publicDir: 'audio',
+  publicDir: 'public',
   server: {
     host: '127.0.0.1',
     port: 5173,
